440d134ec92d3fdcc92904ba10b407d1cfb895c4
artty52 <artty52@gmail.com>
2019-10-22 22:37:32 +0700

    Initial commit

---
+++ array.js
@@ -0,0 +1,19 @@
+// Array Method
+var data = ['A' , 'B' , 'C' , 'D']
+// push
+// เพิ่มสมาชิกต่อท้าย array
+data.push('E')
+console.log(data)
+// ['A' , 'B' , 'C' , 'D' , 'E']
+
+// pop
+// ลบสมาชิกตัวสุดท้ายออก
+var x = data.pop()
+console.log(data)
+// ['A' , 'B' , 'C' , 'D']
+
+// shift
+// ลบสมาชิกตัวสุดแรกออก
+var y = data.shift()
+console.log(data)
+// [B' , 'C' , 'D']
