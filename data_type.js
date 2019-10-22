4d4eea1517f9e74d40c366efad6af24f58c7d4d5
artty52 <artty52@gmail.com>
2019-10-22 22:19:34 +0700

    Initial commit

--- data_type.js
+++ data_type.js
@@ -1,1 +1,31 @@
 console.log('Hello World')
+
+// ชนิดข้อมูล (Data Type)
+// Number, String, Boolean
+
+var id = 3 // Number
+var name = 'Artdvp' //String
+var ok = true // Boolean
+
+var name2 = `Artdvp id = ${id}` // การทำ String ให้สามารถใส่ตัวแปรหรือ expression (Template literals) ครอบด้วย `...`
+
+// Array, Object, Function
+// Object
+/*
+var Object = {
+    key: 'value'
+}
+*/
+var student = {
+    code : '001',
+    name : 'Artdvp'
+}
+
+// แสดงค่า
+console.log(student.code) // 001
+console.log(student[name]) // Artdvp
+// เพิ่ม key ใหม่
+student['age'] = 25
+// ลบ key
+delete student.age
+
